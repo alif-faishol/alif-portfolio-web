3af13a05ad42cf7025ff2668c78671736d37d52b
Alif Faishol <alifaishol@gmail.com>
2017-08-03 20:59:42 +0700

    ...

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router} from 'react-router';
 import {HashRouter, Route} from 'react-router-dom';
 import './index.css';
 import App from './App';
-import TestPage from './TestPage'
+import TestPage from './TestPage';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <HashRouter>
     <div>
-      <Route exact path='/' component={App} />
+      <Route path='/' component={App} />
       <Route path='/test' component={TestPage} />
     </div>
   </HashRouter>, document.getElementById('root'));
