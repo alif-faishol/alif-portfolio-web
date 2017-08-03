fdfc284eb8400460f617215bbd22e5ed8d0328a8
Alif Faishol <alifaishol@gmail.com>
2017-08-03 17:41:46 +0700

    react router

---
+++ src/TestPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      <p>success yo</p>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router} from 'react-router';
+import {HashRouter, Route} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import TestPage from './TestPage'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <HashRouter>
+    <div>
+      <Route exact path='/' component={App} />
+      <Route path='/test' component={TestPage} />
+    </div>
+  </HashRouter>, document.getElementById('root'));
+
 registerServiceWorker();
