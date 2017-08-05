ec9ac9d9440a5484dbb99b8adb564751dcfedf21
Alif Faishol <alifaishol@gmail.com>
2017-08-06 06:42:58 +0700

    centering nav items

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import styled from 'styled-components'
 
-const Container = styled.div`
+const Container = styled.nav`
   position: absolute;
   background-color: #333333;
   top: 20px;
@@ -17,19 +17,38 @@ const Container = styled.div`
 `
 
 const StyledLink = styled(Link)`
-  display: table-cell;
-  vertical-align: middle;
+  position: relative;
   height: 50px;
   font-size: 20px;
   width: 100%;
   color: white;
+  display: block;
   font-family: 'Fira Code', sans-serif;
+  > * {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    margin-right: -50%;
+  }
 `
 
 export default () => {
   return (
     <Container>
-      <StyledLink to='/'><div>&lt;/&gt;</div></StyledLink>
+      <StyledLink to='/'>
+        <div>/</div>
+      </StyledLink>
+      <StyledLink to='/'>
+        <div>/</div>
+      </StyledLink>
+      <StyledLink to='/'>
+        <div>/</div>
+      </StyledLink>
+      <StyledLink to='/'>
+        <div>/</div>
+      </StyledLink>
     </Container>
   )
 }
