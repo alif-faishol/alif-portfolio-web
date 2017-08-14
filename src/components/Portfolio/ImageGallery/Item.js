181a52a41ca66ab4cb61df1d7faf197a18e8e187
Alif Faishol <alifaishol@gmail.com>
2017-08-14 11:17:52 +0700

    image loading

--- src/components/Portfolio/ImageGallery/Item.js
+++ src/components/Portfolio/ImageGallery/Item.js
@@ -18,16 +18,58 @@ const Title = styled.div`
   padding-bottom: 15px;
 `
 
+const Loading = styled.div`
+  display: ${props => props.isLoading ? 'block' : 'none'};
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-color: #eeeeee;
+  color: #777777;
+  div {
+    position: absolute;
+    margin: 0;
+    margin-right: -50%;
+    top: 50%;
+    font-size: 50px;
+    left: 50%;
+    text-align: center;
+    transform: translate(-50%, -50%);
+    p {
+      font-weight: 600;
+      font-size: 20px;
+    } 
+  }
+`
+
+const StyledImg = styled.img`
+  display: ${props => props.isLoading ? 'none' : 'block'};
+  height: 100%;
+  width: 100%;
+  animation: 1s 1 FadeIn;
+  @keyframes FadeIn {
+    from { opacity: 0; }
+    to { opacity: 1; }
+  }
+`
+
 export default class extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      isLoading: true
+      isLoading: true,
+      isFailed: false
     }
+    this.loaded = this.loaded.bind(this)
+    this.failed = this.failed.bind(this)
   }
   loaded = () => {
     this.setState({
       isLoading: false
+    })
+  }
+  failed = () => {
+    this.setState({
+      isFailed: true
     })
   }
   render() {
@@ -37,18 +79,28 @@ export default class extends React.Component {
           {this.props.title}
         </Title>
         <Content thumbnail={this.props.thumbnail}>
-          <div style={{display: this.state.isLoading ? 'block' : 'none'}}>
-            Loading
-          </div>
-          <img
-            style={{
-              visibility: this.state.isLoading ? 'hidden' : 'visible',
-              height: '100%',
-              width: '100%'
-            }}
+          <Loading isLoading={this.state.isLoading}>
+            <div>
+              <span className=
+                {this.state.isFailed
+                    ? 'typcn typcn-times'
+                    : 'typcn typcn-watch'
+                }>
+              </span>
+              <p>
+                {this.state.isFailed
+                    ? 'Can\'t Load Image'
+                    : 'Loading Image'
+                }
+              </p>
+            </div>
+          </Loading>
+          <StyledImg
+            isLoading={this.state.isLoading}
             src={this.props.thumbnail}
             alt="Thumbnail"
-            onLoad={this.state.isLoading ? this.loaded.bind(this) : null}
+            onLoad={this.state.isLoading ? this.loaded : null}
+            onError={this.state.isFailed ? null : this.failed}
           />
         </Content>
       </SquareBox>
